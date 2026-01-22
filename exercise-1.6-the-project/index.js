115e9b50c41b4cbb68abacaa00acf7ffd1a4fb74
Daniel <urbandanielsp@gmail.com>
2026-01-22 17:39:52 -0300

    Exercise 1.6

---
+++ exercise-1.6-the-project/index.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const app = express()
+
+const PORT = process.env.PORT || 3000
+
+app.get('/', (req, res) => {
+  res.send(`
+    <html>
+      <body>
+        <h1>Todo App running in Kubernetes! Made by nejo</h1>
+        <p>Port: ${PORT}</p>
+      </body>
+    </html>
+  `)
+})
+
+app.listen(PORT, () => {
+  console.log(`Server started in port ${PORT}`)
+})
